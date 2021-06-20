73a0ccdffaded16a8a7a1f0775dc03211a432baa
Chris Udemans <chris@udemans.xyz>
2021-06-21 00:28:49 +0200

    added country names

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 async function drawMap() {
+    
+    // Read in data
     const geoData = await d3.json('data/country-shapes.json')
     let parsedData = topojson.feature(geoData, geoData.objects.countries).features
 
     const countryVac = await d3.json('https://raw.githubusercontent.com/simprisms/global_vac_data/main/data/last_vac2.json')
     
+    // Combine datasets
     for (let i = 0; i < countryVac.length; i++) {
         let location = countryVac[i].location
 
@@ -13,7 +16,7 @@ async function drawMap() {
             if (location == country) {
                 parsedData[j] = Object.assign({}, parsedData[j], countryVac[i])
                 break;
-            }
+            } 
         }
     }
     // Create accessor function
@@ -63,28 +66,43 @@ async function drawMap() {
     
     // Create projections and scale ------USE AGAIN IN OTHER PROJECTS
     let projection = d3.geoEqualEarth().scale(1).translate([0, 0]).precision(0);
-      let path = d3.geoPath().projection(projection);
-      let bound = path.bounds(topojson.feature(geoData, geoData.objects.countries));
+    let path = d3.geoPath().projection(projection);
+    let bound = path.bounds(topojson.feature(geoData, geoData.objects.countries));
 
 
-      var scale = 1.1 / Math.max((bound[1][0] - bound[0][0]) / dimensions.boundedWidth,
+    let scale = 1.1 / Math.max((bound[1][0] - bound[0][0]) / dimensions.boundedWidth,
         (bound[1][1] - bound[0][1]) / dimensions.boundedHeight);
-      var transl = [(dimensions.boundedWidth - scale * (bound[1][0] + bound[0][0])) / 2 -25,
-        (dimensions.boundedHeight - scale * (bound[1][1] + bound[0][1])) / 2
-      ];
-      projection.scale(scale).translate(transl);    
-
+    let transl = [(dimensions.boundedWidth - scale * (bound[1][0] + bound[0][0])) / 2 -25,
+        (dimensions.boundedHeight - scale * (bound[1][1] + bound[0][1])) / 2]
     
+    // Update projection
+    projection.scale(scale).translate(transl);    
+
     // Create number formatter for tooltip
     let numberFormatter = d3.format(",.4r") 
 
-    // Tooltips
+
+    // Create tooltip 
+    // const showToolTip = (text, coords) => {
+    //     d3.select('#tooltip').text(text)
+    //         .style('top', projection(coords[1]))
+    //         .style('left', projection(coords[0]))
+    //         .style('display', 'block')
+    // }
+
+    // const hideToolTip = (text, coords) => {
+    //     d3.select('#tooltip').text(text)
+    //         .style('display', 'none')
+    // }
+
+
+    // Tooltip
     const tip = d3.tip()
         .attr("class", "d3-tip")
         .offset([-10, -10])
         .html(d => {
             if (d.location == undefined) {
-                return `<p class="no-data"><strong>No Data</p></strong>`
+                return null
             } else {
                 return `<p id="geo-name"><strong>${d.location}</strong></p><p class="figures">${d.location} has administered <strong>${numberFormatter(d.people_vaccinated)}</strong> doses, <br>covering <strong>${d.total_vaccinations_per_hundred}%</strong> of the country's population.</strong></p>`
             } 
@@ -102,21 +120,14 @@ async function drawMap() {
         .attr("d", path)
         .attr("stroke", "white")
         .attr("stroke-width", "1px")
-        .on('mouseover', function (d) {
-            tip.show(d)
-        })
-        .on("mouseleave", function(d) {
-            tip.hide(d)
-        })
+        .on('mouseover', d => d.location == undefined ? tip.hide(d) : tip.show(d))
+        .on("mouseleave", d => tip.hide(d))
+        // .on("mouseover", d => showToolTip(d.location, [d3.event.clientX, d3.event.clientY]))
+        // .on('mousemove', d => showToolTip(d.location, [d3.event.clientX, d3.event.clientY]))
+        // .on("mouseover", hideToolTip)
         .merge(map)
-            .attr("fill", d => {
-                if (d.total_vaccinations_per_hundred !== undefined) {
-                    return  colors(vacAccessor(d))
-                } else {
-                    return "#cecfc8"
-                }
-                
-            }) 
+            .attr("fill", d => d.total_vaccinations_per_hundred !== undefined ? colors(vacAccessor(d)) : "#cecfc8")
+    
 
     // Create legend
     // const legend = d3.select('#map-legend').append("svg")
@@ -144,7 +155,6 @@ async function drawMap() {
             .attr("fill", () => {
                 while (i < colors.ticks().length) {
                 return colors(colors.ticks()[i])
-                i ++
                 }
             })
 
@@ -156,40 +166,24 @@ async function drawMap() {
             .text(() => {
                 while (i < colors.ticks().length -1) {
                     return colors.ticks()[i]
-                    i ++
                 }
-            } ) 
-    })
-
+            }) 
+        })
 
     // Create labels
-    const countries = ['United States', 'China', 'Brazil', 'Russia', 'Australia',
-        'Algeria', 'India', 'Angola', 'Iran', 'Sudan', 'Greenland']
+    const countries = ['China', 'Brazil', 'Russia', 'Australia',
+        'Algeria', 'India', 'Angola', 'Iran', 'Sudan', 
+        'Greenland', 'Spain', 'Germany', 'Pakistan', 'Kenya', 'Niger', 'Japan', 'Thailand', 'Mexico']
         
     const labels = bounds.selectAll('text')
         .data(newData)
         .enter()
             .append('text')
                 .attr("class", 'country-name')
-                .attr("x", function(d){
-                    if (d.location == 'Iran') {
-                        return path.centroid(d)[0] + 5
-                    } else {
-                        return path.centroid(d)[0] ; 
-                    }
-                })
-                .attr("y", function(d){
-                        return  path.centroid(d)[1] + 5;
-                })
+                .attr("x", d => (d.location == 'Iran' | d.location == 'Greenland') ? path.centroid(d)[0] + 5 : path.centroid(d)[0])
+                .attr("y", d => path.centroid(d)[1] + 5)
                 .attr("text-anchor", "middle")
-                .text(d => {
-                    if (countries.includes(d.location)) {
-                            return d.location
-                        
-                    } else {
-                        return ""
-                    }
-                })
+                .text(d => countries.includes(d.location) ? d.location : "")
         
 }
 
